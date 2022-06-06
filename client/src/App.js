deba927a9f8bb68897629d617ddddd93e045f8c5
Becky Wilson <crhsbandmom@gmail.com>
2022-06-06 18:28:08 -0400

    created footer index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,26 @@
-import Navbar from "./Navbar";
+import Navbar from "./components/Header/Navbar";
+import Footer from "./components/Footer";
+
+//delete this line in future
+const inlineStyle = {
+  height: "400px",
+  backgroundColor: "blue",
+  color: "white",
+  margin: "10px",
+};
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <br />
+      <div style={inlineStyle}>
+        <h1>Please delete this section </h1>
+      </div>
+      <br />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,18 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <a href="#" className="socialIcons">
+        Github
+      </a>
+      <a href="#" className="socialIcons">
+        Instagram
+      </a>
+      <a href="#" className="socialIcons">
+        Twitter
+      </a>
+      <a href="#" className="socialIcons">
+        Facebook
+      </a>
+    </footer>
+  );
+}
