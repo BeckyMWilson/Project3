980986a6669058e8eab75a04d8361c1b4d4e192f
Becky Wilson <crhsbandmom@gmail.com>
2022-06-07 11:01:01 -0400

    added alt tags to social media links

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,18 +1,14 @@
+import React from 'react'
+<link rel="style" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+
 export default function Footer() {
   return (
     <footer className="footer">
-      <a href="#" className="socialIcons">
-        Github
-      </a>
-      <a href="#" className="socialIcons">
-        Instagram
-      </a>
-      <a href="#" className="socialIcons">
-        Twitter
-      </a>
-      <a href="#" className="socialIcons">
-        Facebook
-      </a>
+              <h2>Made by the hard-to-port team.</h2>
+          <a href="#" class="fa fa-facebook" alt="facebook"></a>
+          <a href="#" class="fa fa-twitter" alt="twitter"></a>
+          <a href="#" class="fa fa-linkedin" alt="linkedin"></a>
+          <a href="#" class="fa fa-instagram" alt="instagram"></a>
     </footer>
   );
 }
