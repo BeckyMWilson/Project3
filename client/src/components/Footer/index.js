9fe9b7937393f740c0bce7ec68f28a8173284643
Drew Knoeller <drewk2629@gmail.com>
2022-06-09 15:57:04 -0400

    fake data displayed

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ExternalLink } from 'react-external-link';
+
 import "./FooterStyles.css";
 
 <link rel="style" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
@@ -10,10 +10,10 @@ export default function Footer() {
 
     <footer className="footer">
       <h2>Made by the hard-to-port team.</h2>
-      <a href="https://www.facebook.com" class="fa fa-facebook" alt="facebook" target="_blank"></a>
-      <a href="https://www.twitter.com" class="fa fa-twitter" alt="twitter" target="_blank"></a>
-      <a href="https://www.linkedin.com" class="fa fa-linkedin" alt="linkedin" target="_blank"></a>
-      <a href="https://www.instagram.com" class="fa fa-instagram" alt="instagram" target="_blank"></a>
+      <a href="https://www.facebook.com" className="fa fa-facebook" alt="facebook" target="_blank"></a>
+      <a href="https://www.twitter.com" className="fa fa-twitter" alt="twitter" target="_blank"></a>
+      <a href="https://www.linkedin.com" className="fa fa-linkedin" alt="linkedin" target="_blank"></a>
+      <a href="https://www.instagram.com" className="fa fa-instagram" alt="instagram" target="_blank"></a>
     </footer>
 
   );
